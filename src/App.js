39718ad459869b62820482a2c93b753df90e1f9b
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-01-29 21:18:58 -0700

    Updated header and intro background

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Intro from './components/Intro';
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <Header />
-      </div>
-      <div className='intro'>
-        <Intro /> 
+      <div className="headerAndIntroBackground">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="intro">
+          <Intro />
+        </div>
       </div>
     </div>
   );
