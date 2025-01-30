36efe4461b9f17f25e9321e435cea2f18f5e05ec
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-01-29 21:05:31 -0700

    Added intro & headshot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from "../src/components/Header";
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
       <div className="header">
         <Header />
+      </div>
+      <div className='intro'>
+        <Intro /> 
       </div>
     </div>
   );
---
+++ src/components/Intro.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import headshot from "../images/headshot.jpeg";
+
+
+function Intro() {
+  return (
+    <>
+    <img src={headshot} alt="adult male smiling" />
+      <div>
+        <h2>
+          A Software Developer based in Kalispell, MT, creating seamless digital
+          experiences. Explore my work and let’s discuss your project!
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default Intro;
