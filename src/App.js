179934851c28bb7d6d38e4141955f48910c8600c
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-01-31 09:14:04 -0700

    Added projects, certs, and connect to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from "../src/components/Header";
 import Intro from './components/Intro';
 import AboutMe from './components/AboutMe';
 import Technologies from './components/Technologies';
+import Certificates from './components/Certificates';
+import Projects from './components/Projects';
+import Connect from './components/Connect';
 
 function App() {
   return (
@@ -14,13 +17,37 @@ function App() {
         <div className="intro">
           <Intro />
         </div>
-        </div>
+      </div>
+
+      <div className="about-me-container">
         <div className="about-me">
           <AboutMe />
         </div>
-        <div className='technologies'>
+      </div>
+
+      <div className="techonologies-container">
+        <div className="technologies">
           <Technologies />
         </div>
+      </div>
+
+      <div className="certficates-container">
+        <div className="certificates">
+          <Certificates />
+        </div>
+      </div>
+
+      <div className="projects-container">
+        <div className="projects">
+          <Projects />
+        </div>
+      </div>
+
+      <div className="connect-container">
+        <div className="connect">
+          <Connect />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Connect.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Connect() {
+    return (
+        <div>
+            <h1>Let's Connect Section</h1>
+        </div>
+    );
+}
+
+export default Connect; 
