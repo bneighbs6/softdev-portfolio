02b61ff917b9ea7321457e1fa4f51829c73dbc96
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-03 13:57:43 -0700

    Made 1st project into a card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import lawnScreenshot from "../images/lawn-screenshot.png"
+import { Card, Button } from "react-bootstrap";
 
 function Projects() {
     return (
       <div className="projects">
         <article id="projects">
           <h2 className="major">Projects</h2>
-          <span className="image main">
-            <img src="images/pic02.jpg" alt="" />
+          <Card className="lawn-card">
+            <Card.Img
+              variant="top"
+              src={lawnScreenshot}
+              className="lawn-screenshot"
+            />
+            <Card.Body>
+              <Card.Title>B Neighborly Lawn Services</Card.Title>
+              <Card.Text>
+                Frontend web application for a local small business specializing
+                in lawn care services. Utilizing JavaScript, React, and
+                Bootstrap, the application is designed to enhance user
+                experience and streamline service offerings.
+              </Card.Text>
+              <a 
+              className="project-link"
+              href="https://bneighborlylawns.onrender.com/"
+              target="_blank"
+              rel="noopener noreferrer">
+                <Button>Live Demo</Button>
+              </a>
+              <a 
+              className="project-link"
+              href="https://github.com/bneighbs6/b-neighborly-lawn"
+              target="_blank"
+              rel="noopener noreferrer">
+                <Button>Source Code</Button>
+              </a>
+            </Card.Body>
+          </Card>
+          {/* <span className="image main">
+            <img src={lawnScreenshot} alt="screenshot of a website" className="lawn-screenshot" />
           </span>
           <p>
             Frontend web application for a local small business specializing in
@@ -22,7 +54,7 @@ function Projects() {
             >
               Frontend Lawn Service Website
             </a>
-          </p>
+          </p> */}
           <p>
             Full-stack web application designed for restaurants to be able to
             create, read, update, and delete reservations from their system.
