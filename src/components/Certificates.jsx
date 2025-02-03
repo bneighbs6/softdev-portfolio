89efcf6ed2d77cac2a532f5f9c73230693a719a3
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-02 20:18:00 -0700

    Added list of certificates earned

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 function Certificates() {
     return (
-        <div>
-            <h1>Certificates Section</h1>
-        </div>
+      <div>
+        <h1>Certificates Section</h1>
+        <ul>
+          <li>Software Engineering - Chegg Skills</li>
+          <li>Full Stack Engineering - Udemy</li>
+        </ul>
+      </div>
     );
 }
 
