b4c07aa6d5208d52ee7ab41b722c29799e02ff06
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-02 20:15:45 -0700

    Added list of technologies proficient with

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,6 +4,7 @@ function Technologies() {
     return (
         <div>
             <h1>Technologies Section</h1>
+            <h3>HTML, CSS, Bootstrap CSS, Javascript, Node.js, React, Express, PostgreSQL</h3>
         </div>
     );
 }
