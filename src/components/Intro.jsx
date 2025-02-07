733a0b22e896126e2132a353b5800592f6785898
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-07 11:59:00 -0700

    Edit Projects button in Intro and App.css

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import headshot from "../images/headshot.jpeg";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-scroll";
 
 
 function Intro() {
   return (
     <>
-    <img src={headshot} alt="adult male smiling" />
+      <img src={headshot} alt="adult male smiling" />
       <div>
         <h2>
           A Software Developer based in Kalispell, MT, creating seamless digital
           experiences. Explore my work and let’s discuss your project!
         </h2>
       </div>
-      <button className="projects-btn">Projects</button>
+      <Nav.Link as={Link} to="projects" smooth={true} duration={500}>
+        <button className="projects-btn">
+          Projects
+        </button>
+      </Nav.Link>
     </>
   );
 }
