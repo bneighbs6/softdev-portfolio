0452d2e154408d3ca466e385f6f075e1fb3ac4ff
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-07 11:39:39 -0700

    Update Connect & Connect CSS

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -2,27 +2,37 @@ import React from "react";
 
 function Connect() {
     return (
-        <div>
-            <h3>Let's Connect</h3>
-            <p>via Email, LinkedIn, GitHub</p>
-            <div>
-                <span>
-                    <a href="mailto:bneighbs6@gmail.com" title="Email">
-                        <i className="fas fa-envelope"></i>
-                    </a>
-                </span>
-                <span>
-                    <a href="https://www.linkedin.com/in/brett-neighbors/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                </span>
-                <span>
-                    <a href="https://github.com/your-github-username" title="GitHub" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </span>
-            </div>
+      <div className="connect-container">
+        <h3>Let's Connect</h3>
+        <p>via Email, LinkedIn, GitHub</p>
+        <div className="connect-icons">
+          <span>
+            <a href="mailto:bneighbs6@gmail.com" title="Email">
+              <i className="fas fa-envelope"></i>
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/brett-neighbors/"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://github.com/bneighbs6"
+              title="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </span>
         </div>
+      </div>
     );
 }
 
