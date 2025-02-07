8e952da83b1e6d459bd72d5311969572cb556f0d
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-02-07 11:02:40 -0700

    Add icons for each way to connect w/ me

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -3,9 +3,27 @@ import React from "react";
 function Connect() {
     return (
         <div>
-            <h1>Let's Connect Section</h1>
+            <h3>Let's Connect</h3>
+            <p>via Email, LinkedIn, GitHub</p>
+            <div>
+                <span>
+                    <a href="mailto:bneighbs6@gmail.com" title="Email">
+                        <i className="fas fa-envelope"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="https://www.linkedin.com/in/brett-neighbors/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="https://github.com/your-github-username" title="GitHub" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </span>
+            </div>
         </div>
     );
 }
 
-export default Connect; 
+export default Connect;
