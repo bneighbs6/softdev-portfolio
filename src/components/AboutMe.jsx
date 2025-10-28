1a26c1c32050912f92d9ee6c8e556ba21e8f44fe
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-28 10:59:39 -0600

    Edit icon for About Me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 function AboutMe() {
-    return (
-      <div id="about">
-        <h3>About Me Section</h3>
-        <h5>
-          Full-stack software engineer proficient in JavaScript for both
-          front-end and back-end programming. Well-versed in React, HTML, CSS,
-          API implementation, Node.js, Express, PostgreSQL, and frameworks like
-          Bootstrap. Experience in the strength and conditioning field creates
-          the ability to adapt quickly to new information, and apply skills to
-          handle that information efficiently.
-        </h5>
-      </div>
-    );
+  return (
+    <div id="about">
+      <i className="fa-solid fa-circle-user"></i>
+      <h3>About Me</h3>
+      <h5>
+        Full-stack software engineer proficient in JavaScript for both front-end
+        and back-end programming. Well-versed in React, HTML, CSS, API
+        implementation, Node.js, Express, PostgreSQL, and frameworks like
+        Bootstrap. Experience in the strength and conditioning field creates the
+        ability to adapt quickly to new information, and apply skills to handle
+        that information efficiently.
+      </h5>
+    </div>
+  );
 }
 
-export default AboutMe; 
+export default AboutMe;
