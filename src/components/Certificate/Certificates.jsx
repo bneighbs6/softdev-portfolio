90d66e04ca728145b23af503e4bed78fe556db07
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-11-02 15:05:14 -0700

    Update Certificate icon

--- src/components/Certificate/Certificates.jsx
+++ src/components/Certificate/Certificates.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAward } from "@fortawesome/free-solid-svg-icons";
 import "./Certificates.css";
 
 function Certificates() {
     return (
       <div id="certificates">
+        <FontAwesomeIcon icon={faAward} className="certificate-icon" />
         <h1>Certificates Section</h1>
         <ul>
           <li>Software Engineering - Chegg Skills</li>
