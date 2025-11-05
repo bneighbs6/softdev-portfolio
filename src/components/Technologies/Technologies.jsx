8433df3e23ca612c099d4adc69b2caf81d1c5e1f
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-11-05 11:38:10 -0700

    Add icon and style to technologies section

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Technologies.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 
 function Technologies() {
     return (
         <div id="technologies">
+            <FontAwesomeIcon icon={faLaptopCode} className="techonology-icon" />
             <h1>Technologies Section</h1>
             <h3>HTML, CSS, Bootstrap CSS, Javascript, Node.js, React, Express, PostgreSQL</h3>
         </div>
