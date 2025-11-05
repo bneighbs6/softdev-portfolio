da84f5f9f4331fec0ea2d7997e658c783b7f818c
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-11-05 11:34:17 -0700

    Alteranate bg colors for each section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,24 +4,22 @@ import { Nav } from "react-bootstrap";
 import { Link } from "react-scroll";
 import "./Intro.css";
 
-
 function Intro() {
   return (
     <div id="intro">
       <img src={headshot} alt="adult male smiling" />
-      <div>
+      <div className="intro-description-div">
         <h2>
           A Software Developer based in Kalispell, MT, creating seamless digital
           experiences. Explore my work and let’s discuss your project!
         </h2>
+        <Nav.Link as={Link} to="projects" smooth={true} duration={500}>
+          <button className="projects-btn">Projects</button>
+        </Nav.Link>
       </div>
-      <Nav.Link as={Link} to="projects" smooth={true} duration={500}>
-        <button className="projects-btn">
-          Projects
-        </button>
-      </Nav.Link>
     </div>
   );
 }
 
 export default Intro;
+
