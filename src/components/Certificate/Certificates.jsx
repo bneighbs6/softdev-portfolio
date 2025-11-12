386fe7c6cca6e0a9410afa55011a0330923ea924
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-11-12 11:30:25 -0700

    Make each certificate its own Card

--- src/components/Certificate/Certificates.jsx
+++ src/components/Certificate/Certificates.jsx
@@ -2,16 +2,35 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAward } from "@fortawesome/free-solid-svg-icons";
 import "./Certificates.css";
+import { Card, Row, Col } from "react-bootstrap";
 
 function Certificates() {
     return (
       <div id="certificates">
-        <FontAwesomeIcon icon={faAward} className="certificate-icon" />
-        <h1>Certificates Section</h1>
-        <ul>
-          <li>Software Engineering - Chegg Skills</li>
-          <li>Full Stack Engineering - Udemy</li>
-        </ul>
+        <div className="certificates-container">
+          <FontAwesomeIcon icon={faAward} className="certificate-icon" />
+          <h1>Certificates</h1>
+          <Row className="align-items-stretch">
+            <Col xs={12} md={6} className="mb-4 d-flex">
+              <Card className="universal-card h-100 w-100">
+                <Card.Body>
+                  <Card.Title>Chegg Skills</Card.Title>
+                  <hr />
+                  <Card.Text>Engineering Flex</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={6} className="mb-4 d-flex">
+              <Card className="universal-card h-100 w-100">
+                <Card.Body>
+                  <Card.Title>Udemy</Card.Title>
+                  <hr />
+                  <Card.Text>Full Stack Engineering</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
 }
